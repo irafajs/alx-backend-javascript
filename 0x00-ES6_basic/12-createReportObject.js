cbcd75ef06a1cea86bb81d4067e983f9ff9067d3
irafajs <irafajs@gmail.com>
2024-03-05 00:32:01 +0200

    ALL TASKS

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  const getNumberOfDepartments = () => Object.keys(employeesList).length;
+
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments
+  };
+}
