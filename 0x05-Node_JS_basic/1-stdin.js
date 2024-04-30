d73af5ce3af75623e4c53b723604a0ab0487247f
Irafasha Jules <jirafasha@ubuntud.myguest.virtualbox.org>
2024-04-30 16:09:33 +0200

    TASK01 UPDATE06

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  process.stdout.write(`Your name is: ${input}\n`);
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
 process.stdin.on('end', () => {
