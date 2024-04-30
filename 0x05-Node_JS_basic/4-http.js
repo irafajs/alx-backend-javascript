105a209a7b02a9cb99ca24aadc487bb8253e7976
Irafasha Jules <jirafasha@ubuntud.myguest.virtualbox.org>
2024-05-01 01:51:55 +0200

    TASK04 UPDATE03

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,14 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const app = http.createServer();
 
-  res.end('Hello Holberton School!\n');
+app.on('request', (_, res) => {
+  const showtext = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', showtext.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(showtext));
 });
 
 app.listen(1245, () => {
