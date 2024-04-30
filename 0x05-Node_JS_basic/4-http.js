148498c5cf41a169ed8791b41b0f1853b673c02d
Irafasha Jules <jirafasha@ubuntud.myguest.virtualbox.org>
2024-05-01 01:40:53 +0200

    TASK04 UPDATE01

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,7 +7,7 @@ const app = http.createServer((req, res) => {
 });
 
 app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+  process.stdout.write('Server is running on port 1245');
 });
 
 module.exports = app;
