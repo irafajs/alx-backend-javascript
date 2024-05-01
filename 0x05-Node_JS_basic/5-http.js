bae623c1cc5145864784d5019605b2e912e87fbd
Irafasha Jules <jirafasha@ubuntud.myguest.virtualbox.org>
2024-05-01 02:14:26 +0200

    TASK05

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer();
+
+app.on('request', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    const showtext = 'Hello Holberton School!';
+
+    res.setHeader('Content-Length', showtext.length);
+    res.statusCode = 200;
+    res.write(Buffer.from(showtext));
+  } else if (req.url === '/students') {
+    const databasePath = 'database.csv';
+    countStudents(databasePath)
+      .then((output) => {
+        res.statusCode = 200;
+        res.end(output);
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+        console.error(error);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('404 Not Found\n');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
+
+module.exports = app;
