cf75c222bdcb3d48bda27f55c89fac52b77b37af
Irafasha Jules <jirafasha@ubuntud.myguest.virtualbox.org>
2024-05-05 14:34:46 +0200

    TASK01

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return the sum of two rounded numbers', function() {
+      assert.equal(calculateNumber('SUM', 1.4, 2.6), 4);
+    });
+  });
+
+  describe('SUBTRACT', function() {
+    it('should return the difference of two rounded numbers', function() {
+      assert.equal(calculateNumber('SUBTRACT', 5.6, 3.2), 3);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return the division result of two rounded numbers', function() {
+      assert.equal(calculateNumber('DIVIDE', 8.2, 2), 4);
+    });
+
+    it('should return "Error" if second number is rounded to 0', function() {
+      assert.equal(calculateNumber('DIVIDE', 8.2, 0), 'Error');
+        });
+    });
+
+	describe('Invalid Type', function() {
+    it('should throw an error for an invalid type', function() {
+      assert.throws(() => calculateNumber('INVALID_TYPE', 1, 2), Error);
+    });
+  });
+});
