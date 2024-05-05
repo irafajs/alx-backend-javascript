29a26fc4e59a242c9d6392cb03702e75ad9393ca
Irafasha Jules <jirafasha@ubuntud.myguest.virtualbox.org>
2024-05-05 19:19:49 +0200

    TASK05

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  let consoleSpy;
+
+  beforeEach(function() {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    consoleSpy.restore();
+  });
+
+  it('verify that 100 + 20 is 120 and console is called one', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+
+  it('verify that 10 + 10 is 20 and consle is called once', function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+
+});
