015dc4d9cce922c1b0b18d35e3a82cd4c257ea9a
Irafasha Jules <jirafasha@ubuntud.myguest.virtualbox.org>
2024-05-05 13:42:06 +0200

    TASK00 UPDATE03

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,46 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', () => {
-  it('should return the sum of two rounded numbers', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+  it('test the first number rounding', () => {
+    assert.equal(calculateNumber(1.2, 3), 4);
   });
 });
+
+describe('calculateNumber', () => {
+  it('tests the second number rounding', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+});
+
+describe('calculateNumber', () => {
+  it('tests both number rounding', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+});
+
+
+describe('calculateNumber', () => {
+  it('test both number zero and the right outcome', () => {
+    assert.equal(calculateNumber(0, 0), 0);
+  });
+});
+
+describe('calculateNumber', () => {
+  it('test first number negative', () => {
+    assert.equal(calculateNumber(-1.6, 0), -2);
+  });
+});
+
+describe('calculateNumber', () => {
+  it('test second number negative', () => {
+    assert.equal(calculateNumber(0, -1.3), -1);
+  });
+});
+
+describe('calculateNumber', () => {
+  it('test both number negative', () => {
+    assert.equal(calculateNumber(-1.6, -2.9), -5);
+  });
+});
+
